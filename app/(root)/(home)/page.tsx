207f2105b2eccdd5ecbcc43e7a04d6baf0c95b3e
Mohd Amaan <mhdamaan79@gmail.com>
2025-06-29 20:37:07 +0530

    initial project setup

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="bg-red-500 text-yellow">
+      Home
+      <div className="bg-red-500 text-white p-10">Test Tailwind</div>
+    </div>
+  );
+};
+
+export default Home;
