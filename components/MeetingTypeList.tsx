fbacf54fa1288309eac84bc896ef48e5e5caee15
Mohd Amaan <mhdamaan790@gmail.com>
2025-07-05 18:42:10 +0530

    integrating stream sdk

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -5,21 +5,63 @@ import HomeCard from "./HomeCard";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import MeetingModal from "./MeetingModal";
+import { useUser } from "@clerk/nextjs";
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
 
 const MeetingTypeList = () => {
   const router = useRouter();
   const [meetingState, setMeetingState] = useState<
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >();
+  const { user } = useUser();
+  const client = useStreamVideoClient();
+  const [values, setValues] = useState({
+    dateTime: new Date(),
+    description: "",
+    link: "",
+  });
 
-  const createMeeting = () => {};
+  const [callDetails, setCallDetails] = useState<Call>();
+
+  const createMeeting = async () => {
+    if (!client || !user) return;
+
+    try {
+      const id = crypto.randomUUID();
+      const call = client.call("default", id);
+
+      if (!call) throw new Error("Failed to create call");
+      const startsAt =
+        values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+      const description = values.description || "Instant Meeting";
+
+      await call.getOrCreate({
+        data: {
+          starts_at: startsAt,
+          custom: {
+            description,
+          },
+        },
+      });
+
+      setCallDetails(call);
+
+      console.log("call.id:", call.id);
+
+      if (!values.description) {
+        router.push(`/meeting/${call?.id}`);
+      }
+    } catch (error) {
+      console.error("Create meeting error:", error);
+    }
+  };
 
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
         imgPath="/icons/add-meeting.svg"
         title="New Meeting"
-        description="Start and instant meeting"
+        description="Start an instant meeting"
         handleClick={() => setMeetingState("isInstantMeeting")}
         className="bg-orange-gradient"
       />
